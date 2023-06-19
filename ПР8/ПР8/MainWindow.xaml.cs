3407f47b6f73797c1264d690a8ef92b18af0cf36
FaridJan <126804007+1321fFard@users.noreply.github.com>
2023-06-19 22:18:31 +0300

    Add files via upload

---
+++ ПР8/ПР8/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace LightingApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var gradientAnimation = new PointAnimation
+            {
+                From = new Point(0.3, -0.3),
+                To = new Point(0, 1.5),
+                Duration = TimeSpan.FromSeconds(2),
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+
+                        var brush = (RadialGradientBrush)ball.Fill;
+            brush.BeginAnimation(RadialGradientBrush.GradientOriginProperty,
+gradientAnimation);
+        }
+    }
+}
